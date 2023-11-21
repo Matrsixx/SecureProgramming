55df6a69fb61e0934bb64e7b64e4bc3b50df53b7
matrsixx <vincent.martanu@binus.ac.id>
2023-11-22 01:26:11 +0700

    add transaction

---
+++ actions/doTransaction.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+    include_once '../config/database.php';
+    include_once '../models/Transactions.php';
+    include_once '../utils/encrypt.php';
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        header('Location: ../views/Transaction.php?error=1');
+        $_SESSION['error_message'] = "Wrong Request Method!";
+        exit();
+    }
+
+    if(!isset($_SESSION['token'])){
+
+        $_SESSION['error_message'] = "login first!";
+        header('Location: ../index.php');
+        exit();
+    }
+
+    // $x = new Encrypt();
+    // $x -> decodeJWT($_SESSION['token']);
+    // $id = x->getId();
+    $id = 7;
+
+    $_SESSION['count'] = 0;
+    if(isset($_POST['ongoingsubmit']) && isset($id)){
+        
+        $Data = Transaction::getInstance()->getTransaction($_POST['ongoingsubmit'], $id);
+        $_SESSION['Data'] = array();
+
+        
+        while($row = $Data->fetch_assoc()){
+            $temp = array($row['TransactionDate'], $row['TransactionPrice'], $row['name'], $row['address'], $row['Photo'], $row['phone']);    
+            array_push($_SESSION['Data'], $temp);
+            $_SESSION['count']++;
+        } 
+        
+        header('Location: ../views/Transaction.php');
+
+    }else if(isset($_POST['pastsubmit']) && isset($id)){
+        $Data = Transaction::getInstance()->getTransaction($_POST['pastsubmit'], $id);
+        $_SESSION['Data'] = array();
+
+        while($row = $Data->fetch_assoc()){
+            $temp = array($row['TransactionDate'], $row['TransactionPrice'], $row['name'], $row['address'], $row['Photo'], $row['phone']);    
+            array_push($_SESSION['Data'], $temp);
+            $_SESSION['count']++;
+        }
+      
+        header('Location: ../views/Transaction.php');
+    }else{
+        header('Location: ../views/Transaction.php?error=1');
+        $_SESSION['error_message'] = "login first!";
+        exit();
+    }
+?>
--- views/transaction.php
+++ views/transaction.php
@@ -12,53 +12,39 @@
     </header>
 
     <div class="tab">
-      <button class="tablinks" id="lefttab" onclick="openTab(event, 'tab1')"><h3>Ongoing Transaction</h3></button>
-      <button class="tablinks" id="righttab" onclick="openTab(event, 'tab2')"><h3>Past Transaction</h3></button>
-
-      <div id="tab1" class="tabcontent">
+    
+      <form action="../actions/doTransaction.php" method="POST">
+        <button class="tablinks" id="lefttab" type="submit" value="Ongoing" name="ongoingsubmit"><h3>Ongoing Transaction</h3></button>
+        <button class="tablinks" id="righttab" type="submit" value="Past" name="pastsubmit"><h3>Past Transaction</h3></button>
+      </form>
+      <!-- <div id="tab1" class="tabcontent"> -->
         <div>
           <?php
-              include "Includes/db.php";
-
               session_start();
-              $id = $_SESSION['id'];
-              $query = "SELECT * FROM transactionheader JOIN tenant ON transactionheader.TenantId = tenant.id JOIN users ON transactionheader.usersid = users.id WHERE transactionheader.TransactionProgress = 0 AND transactionheader.usersid = $id;";
-
-              $select_all_query = mysqli_query($connection, $query);
-
-              while($row = mysqli_fetch_assoc($select_all_query)){
-                $tenant_name = $row['name'];
-                $tenant_address = $row['address'];
-                $tenant_photo = $row['Photo'];
-                $tenant_phone = $row['phone'];
-                $transaction_id = $row['TransactionId'];
-                $transaction_date = $row['TransactionDate'];
-                $transaction_progress = $row['TransactionProgress'];
-                $transaction_price = $row['TransactionPrice'];
+              for($i = 0; $i < $_SESSION['count']; $i++){       
             ?>
             <div class="laundry container">
               <div class="leftcolumn">
-                <img src="<?php echo $tenant_photo ?>" alt="Laundry 1">
+                <!-- <img src="<?php echo $_SESSION['Data'][$i][5]?>" alt="Laundry 1">  -->
               </div>
               <div class="middlecolumn">
                 <div class="info">
-                  <h3><?php echo $tenant_name ?></h3>
-                  <p><?php echo $tenant_address ?></p>
-                  <p><?php echo $tenant_phone ?></p>
+                  <h3><?php echo $_SESSION['Data'][$i][2] ?></h3>
+                  <p><?php echo $_SESSION['Data'][$i][3] ?></p>
                 </div>
               </div>
               <div class="rightcolumn">
                 <div class="info">
                   <p><?php 
-                  $date = strtotime($transaction_date);
+                  $date = strtotime($_SESSION['Data'][$i][0]);
                   echo date("d - m - Y", $date);
                   ?></p>
                   <p><?php 
-                    if ($transaction_progress == 0) {
-                      echo "On Progress";
-                    }
+                    // if ($transaction_progress == 0) {
+                    //   echo "On Progress";
+                    // }
                   ?></p>
-                  <p><?php echo 'Rp ', $transaction_price?></p>
+                  <p><?php echo 'Rp ', $_SESSION['Data'][$i][1]?></p>
                 </div>
               </div>
               <button id="order-received-btn" onclick="onChangeProgress(<?php echo $transaction_id; ?>)">Order Received</button>
@@ -67,116 +53,7 @@
         </div>
       </div>
 
-      <div id="tab2" class="tabcontent">
-      <div>
-          <?php
-              include "Includes/db.php";
-
-              $id = $_SESSION['id'];
-              $query = "SELECT * FROM transactionheader JOIN tenant ON transactionheader.TenantId = tenant.id JOIN users ON transactionheader.usersid = users.id WHERE transactionheader.TransactionProgress = 1 AND transactionheader.usersid = $id;";
-
-              $select_all_query = mysqli_query($connection, $query);
-
-              while($row = mysqli_fetch_assoc($select_all_query)){
-                $tenant_name = $row['name'];
-                $tenant_address = $row['address'];
-                $tenant_photo = $row['Photo'];
-                $tenant_phone = $row['phone'];
-                $transaction_date = $row['TransactionDate'];
-                $transaction_progress = $row['TransactionProgress'];
-                $transaction_price = $row['TransactionPrice'];
-            ?>
-            <div class="laundry container">
-              <div class="leftcolumn">
-                <img src="<?php echo $tenant_photo ?>" alt="Laundry 1">
-              </div>
-              <div class="middlecolumn">
-                <div class="info">
-                  <h3><?php echo $tenant_name ?></h3>
-                    <p><?php echo $tenant_address ?></p>
-                    <p><?php echo $tenant_phone ?></p>
-                </div>
-              </div>
-              <div class="rightcolumn">
-                <div class="info">
-                  <p><?php 
-                  $date = strtotime($transaction_date);
-                  echo date("d - m - Y", $date);
-                  ?></p>
-                  <p><?php 
-                    if ($transaction_progress == 1) {
-                      echo "Order Completed";
-                    }
-                  ?></p>
-                  <p><?php echo 'Rp ', $transaction_price?></p>
-                </div>
-              </div>
-            </div>
-
-            <?php } ?>
-        </div>
-      </div>
-    </div>
-
-    <?php
-      include "Includes/db.php";
-
-      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $id = $_POST['id'];
-
-        $query = "UPDATE transactionheader SET TransactionProgress = 1 WHERE TransactionId = $id;";
-        $update_query = mysqli_query($connection, $query);
-
-        // if ($update_query) {
-        //   echo "Success";
-        // } else {
-        //   echo "Error";
-        // }
-      }
-    ?>
-
-    
-    <script>
-        function openDefaultTab() {
-          document.getElementById("lefttab").click();
-        }
-
-        function openTab(evt, tabName) {
-          var i, tabcontent, tablinks;
-          tabcontent = document.getElementsByClassName("tabcontent");
-          for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-          }
-          tablinks = document.getElementsByClassName("tablinks");
-          for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-          }
-          document.getElementById(tabName).style.display = "block";
-          evt.currentTarget.className += " active";
-        }
-
-        document.addEventListener("DOMContentLoaded", openDefaultTab);
-
-        function onChangeProgress(id) {
-          var xhr = new XMLHttpRequest();
-          var url = 'Transaction.php';
-          var method = 'POST';
-          var data = new FormData();
-          data.append('id', id);
-          xhr.open(method, url, true);
-
-          xhr.onload = function() {
-            if (xhr.status === 200) {
-              location.reload();
-            } else {
-              console.error('Error: ' + xhr.status);
-            }
-          };
-
-          xhr.send(data);
-        }
-
-  </script>
+ 
 </body>
 
 </html>
